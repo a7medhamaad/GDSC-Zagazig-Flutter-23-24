6c95629bb0ac60d464923ed8f94152491382b112
a7medhamaad <146642792+a7medhamaad@users.noreply.github.com>
2024-03-01 02:53:21 +0200

    First Task

--- first task/problem3.dart
+++ first task/problem3.dart
@@ -2,3 +2,7 @@ void main() {
   const int myConstant = 7;
   print(myConstant);
 }
+
+//###################################################
+//Problem 3:
+// Declare constant type of int and set value 7.
