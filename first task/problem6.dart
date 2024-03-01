53978fbbaefa295827f3cee5df732ab56beba5c8
a7medhamaad <146642792+a7medhamaad@users.noreply.github.com>
2024-03-01 02:56:21 +0200

    First Task

--- first task/problem6.dart
+++ first task/problem6.dart
@@ -10,3 +10,7 @@ void main() {
     print("$num is odd.");
   }
 }
+//#######################################
+//Problem 6:
+ //Ask the user for a number. Depending on whether the number 
+  //is even or odd, print out an appropriate message to the user.
