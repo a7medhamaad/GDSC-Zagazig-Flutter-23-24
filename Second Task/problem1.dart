5b77ab61a3a6a577a0e07706ae0ef2a9d8f42b5f
a7medhamaad <146642792+a7medhamaad@users.noreply.github.com>
2024-03-01 02:42:26 +0200

    task 1

--- Second Task/problem1.dart
+++ Second Task/problem1.dart
@@ -1,3 +1,11 @@
+//problem 1. Shopping Cart
+//Create a function calculateTotalPrice(cart) that takes a Map
+//representing a shopping cart(item name as key, Prices as value)
+//and returns the total price.Handle cases where items are not
+//found in the Prices Map.
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
 double calculateTotalPrice(Map<String, double> cart, Map<String, double> prices) {
   double totalPrice = 0.0;
 
