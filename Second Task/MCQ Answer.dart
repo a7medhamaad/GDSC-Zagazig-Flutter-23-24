09417c24154591ad218e945efee99cd9447fb8a3
a7medhamaad <146642792+a7medhamaad@users.noreply.github.com>
2024-03-01 02:38:02 +0200

    MCQ Answer.dart

--- Second Task/MCQ Answer.dart
+++ Second Task/MCQ Answer.dart
@@ -1,5 +1,20 @@
-1) var mymap = {key: value1, key2: value2};
-2) All of the above
-3) To pass data back to the caller when the function finishes
-4) while loop
-5) map applies a function to each element, while forEach executes a statement
+//1. What is the correct syntax to declare a Map in Dart?
+    ANSWER) var mymap = {key: value1, key2: value2};
+//#####################################################################################
+2. How can you iterate over the elements of a List in Dart?
+(a) Use a for loop with an index. 
+(b) Use a while loop with a condition. 
+(c) Use the forEach method of the List object. 
+(d) All of the above.
+  
+        ANSWER) All of the above
+//########################################################################################
+3. What is the purpose of the return statement in a function?
+        ANSWER) To pass data back to the caller when the function finishes
+//#########################################################################################
+4. Which control flow statement is used to execute a block of code 
+repeatedly until a condition is met?
+            ANSWER) while loop
+//######################################################################################
+5. What is the difference between a List.map and a List.forEach method?
+       ANSWER) map applies a function to each element, while forEach executes a statement
